845dc77518a95d0d0d7e12b6a3e4c84609de5729
ハムスケ ハム <daiki.esdfth12488@gmail.com>
2023-08-16 00:34:54 +0900

    first

---
+++ src/client/java/com/hamusuke/coop4mc/Coop4MCClient.java
@@ -0,0 +1,9 @@
+package com.hamusuke.coop4mc;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Coop4MCClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/hamusuke/coop4mc/Coop4MC.java
@@ -0,0 +1,14 @@
+package com.hamusuke.coop4mc;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Coop4MC implements ModInitializer {
+	public static final String MOD_ID = "coop4mc";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
